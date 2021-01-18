ada254555a711c73ad81729a8bc5557867fa456d
larryhassan <luna0072018@gmail.com>
2021-01-18 04:39:37 +0000

    add author profile

---
+++ fil2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Author :- Quadri Hassan"
+echo """
+ About the author Quadri (as larry by other name) he's one of edureka study
+we're have been learning DevOps tools in other to become one of great IT in future,
+and alonge this enjoying i meet different people some become friend and were some become family.Recenltly 
+my wife, she's carrier our first baby boy, which i can't wait to meet him he's just like my photo copy.
+And he's always makes me happy because he give me more reason to be happy. Right now i have nothing really to do
+as job."""
+
+var1="Larry world"
+greeting="You're all welcome"
+
+echo "$greeting to $var1"
+
+age=32
+((result=$age*12))
+
+echo $age
+echo "have use $result months on earth!"
+
