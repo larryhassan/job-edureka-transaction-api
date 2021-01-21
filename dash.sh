6f1cb858202e30c58c3781ca9059125e7fd210f4
larryhassan <luna0072018@gmail.com>
2021-01-21 02:00:09 +0000

    remodify space dash file

--- dash.sh
+++ dash.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -x
 echo "The total disk allocation for this system is: "
 echo -e "\n"
 df -h 
 echo -e "\n
+set +x
 df -h | grep /$ | awk '{print "Space left on root partition: " $4}'
